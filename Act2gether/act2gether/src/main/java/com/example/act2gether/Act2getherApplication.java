f654ba1a38930abd8712d29b6d8965d3e7a9aed0
조수영 <josuyeong@josuyeong-ui-MacBookPro.local>
2025-07-08 21:42:07 +0900

    maven으로 수정 및 controller, login 등 추가

---
+++ Act2gether/act2gether/src/main/java/com/example/act2gether/Act2getherApplication.java
@@ -0,0 +1,13 @@
+package com.example.act2gether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Act2getherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Act2getherApplication.class, args);
+	}
+
+}
---
+++ Act2gether/act2gether/src/test/java/com/example/act2gether/Act2getherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.act2gether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Act2getherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
