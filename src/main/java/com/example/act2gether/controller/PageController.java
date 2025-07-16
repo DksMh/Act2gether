d16833aa9c42fd415a89b48b672ff8521bf5629e
DksMh <mhyu95@naver.com>
2025-07-16 17:33:39 +0900

    login_onboarding_page_add

--- src/main/java/com/example/act2gether/controller/PageController.java
+++ src/main/java/com/example/act2gether/controller/PageController.java
@@ -8,25 +8,31 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Controller
 public class PageController {
-    //로그인
-    @GetMapping(value = { "/login" })
+	// 로그인
+	@GetMapping(value = { "/login" })
 	public String login() {
 		return "login";
 	}
-    
-    // 메인
+
+	// 메인
 	@GetMapping(value = { "/" })
 	public String main() {
 		return "main";
 	}
 
-    // qna
+	// 관심사 온보딩 페이지
+	@GetMapping("/onboarding")
+	public String onboarding() {
+		return "interestonboarding"; // interestonboarding.html을 반환
+	}
+
+	// qna
 	@GetMapping(value = { "/page/qna" })
 	public String getQna() {
 		return "qna";
 	}
 
-    // faq
+	// faq
 	@GetMapping(value = { "/page/faq" })
 	public String getFaq() {
 		return "faq";
